f0987ba33dbb928e13be1e0ebec5968e175c1a5e
Stanley Purwahariyanto <stanleypurwaharianto@yahoo.com>
2018-02-06 11:59:02 +0700

    Tes1

---
+++ File.js
@@ -0,0 +1,21 @@
+setTimeout(function() {
+    console.log('World!');
+    }, 2000);
+    console.log('Hello');
+
+    console.log(__dirname);
+    //menampilkan directory path
+
+    console.log(__filename);
+    //Menampilkan File Path
+
+var nama = 'Andi';
+console.log(nama);
+
+var usia;
+usia = 22
+usia = 32
+console.log(usia);
+
+let jomblo = true;
+console.log(jomblo);
